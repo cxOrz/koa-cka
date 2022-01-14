fafee9998adb49f8a36a55dc63799f73b4a9c0ca
miaochenxi <miaochenxi@outlook.com>
2022-01-15 00:06:09 +0800

    tomorrow finish copy template func

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
-import chalk from 'chalk'
-import fs from 'fs/promises'
+const chalk = require('chalk')
+const { Command } = require('commander');
+const { createProject } = require('./lib/createProject');
 
-console.log(`Hello I am ${chalk.green('mcx')}`)
+const program = new Command(chalk.yellow('koa'))
 
+program.version('1.0.0')
+program.description(`You can create a project in directory named ${chalk.blueBright('myproj')} by running ${chalk.blueBright('koa create myproj')}`)
+program.usage(chalk.green('create <project-directory>'))
+program.command('create <project-directory>')
+  .description('create a project')
+  .action((name) => {
+    createProject(name)
+  });
+
+program.parse(process.argv)
